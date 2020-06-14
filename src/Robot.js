ec9ddf414a3758cddda29970eb5ef7439ae6b895
Ian Woolf <dev@ianwoolf.me.uk>
2020-06-14 20:29:07 +0100

    Add an anstract robot class to house all of the common position/orientation stuff

---
+++ src/Robot.js
@@ -0,0 +1,50 @@
+/*
+Abstract parent class for all robot types
+*/
+
+module.exports = class Robot {
+    constructor() {
+        if (new.target === Robot) {
+            throw new Error("Cannot construct Robot instances directly");
+        }
+
+        this._position = {
+            x: undefined,
+            y: undefined
+        }
+        // take 0 to be in the positive y direction (i.e. upwards on a building)
+        // then 90 is facing right, 180 is facing down and 270 is facing left
+        this._orientation = 0;
+    }
+
+    set position(pos) {
+        this._position.x = pos.x;
+        this._position.y = pos.y;
+    }
+
+    get position() {
+        return this._position;
+    }
+
+    set orientation(angle) {
+        if (angle >= 0 && angle <= 360) {
+            this._orientation = angle;
+        }
+        else {
+            throw new Error(`invalid orientation supplied`);
+        }
+    }
+
+    get orientation() {
+        return this._orientation;
+    }
+
+    reportPosition() {
+        console.log(`Current position is (${this.position.x}, ${this.position.y}). Orientation is ${this.orientation}.`);
+    }
+
+    control(position, cmdSeq) {
+        throw new Error(`Child class must overload control method`)
+    }
+
+}
